117493fd2570e54abaabb51930a101de5d929570
Gabriel RIGO <grigo@e1r12p18.42.fr>
2020-09-18 16:37:56 +0200

    jour 7

--- srcs/main.c
+++ srcs/main.c
@@ -213,7 +213,6 @@ char 	**ft_unset(char **cmd, char **env)
 	int idx;
 	char **tmp;
 	char *tcmd;
-	int err;
 
 	tmp = NULL;
 	if ((error = ft_checkexport(cmd)) != 0)
@@ -227,7 +226,6 @@ char 	**ft_unset(char **cmd, char **env)
 	while (cmd[i])
 	{
 		j = 0;
-		err = 0;
 		while (cmd[i][j])
 		{
 			if (!ft_isalnum(cmd[i][j]))
@@ -235,16 +233,17 @@ char 	**ft_unset(char **cmd, char **env)
 				write(1, "unset: ", 7);
 				write(1, cmd[i], ft_strlen(cmd[i]));
 				write(1, ": invalid parameter name\n", 25);
-				err = 1;
+				j = -1;
+				break ;
 			}
 			j++;
 		}
-		if (err == 0)
+		if (j != -1)
 		{
 			if (!(tcmd = (char *)malloc(sizeof(char *) * (ft_strlen(cmd[i]) + 2))))
 				return (NULL);
 			j = 0;
-			while (cmd[j])
+			while (cmd[i][j])
 			{
 				tcmd[j] = cmd[i][j];
 				j++;
@@ -253,20 +252,25 @@ char 	**ft_unset(char **cmd, char **env)
 			tcmd[j + 1] = '\0';
 			if ((idx = ft_checkunset(tcmd, env) - 1) != -1)
 			{
-				if (!(tmp = (char **)malloc(sizeof(char) * (tab_len(env)))))
+				if (!(tmp = (char **)malloc(sizeof(char **) * (tab_len(env)))))
 					return (NULL);
 				j = 0;
-				while(env[j])
+				while(j < idx)
 				{
-					if (j != idx)
-					{
-						if (!(tmp[j] = (char*)malloc(sizeof(char *) * (ft_strlen(env[j]) + 1))))
-							return (NULL);
-						ft_strcpy(tmp[j], env[j]);
-					}
+					if (!(tmp[j] = (char *)malloc(sizeof(char *) * (ft_strlen(env[j]) + 1))))
+						return (NULL);
+					ft_strcpy(tmp[j], env[j]);
 					j++;
 				}
-				tmp[j] = NULL;
+				j++;
+				while (j < tab_len(env))
+				{
+					if (!(tmp[j-1] = (char *)malloc(sizeof(char*) * ft_strlen(env[j]) + 1)))
+						return (NULL);
+					ft_strcpy(tmp[j-1], env[j]);
+					j++;
+				}
+				tmp[j-1] = NULL;
 				ft_splitdel(&env);
 				if (!(env = (char **)malloc(sizeof(char **) * (tab_len(tmp) + 1))))
 					return (NULL);
@@ -282,6 +286,7 @@ char 	**ft_unset(char **cmd, char **env)
 				ft_splitdel(&tmp);
 				tmp = NULL;
 			}
+			free(tcmd);
 		}
 		i++;
 	}
@@ -296,7 +301,6 @@ char 	**ft_export(char **cmd, char **env)
 	char 	**tmp;
 	int 	len;
 
-	i = 1;
 	tmp = NULL;
 	if ((error = ft_checkexport(cmd)) != 0)
 	{
@@ -305,68 +309,90 @@ char 	**ft_export(char **cmd, char **env)
 		write(1, " not found\n", 11);
 		return (env);
 	}
+	i = 1;
 	while (cmd[i])
 	{
-		if (cmd[i][0] == '=')
+		j = 0;
+		while (cmd[i][j])
 		{
-			if (ft_strlen(cmd[i]) == 1 && i == 1)
-				write(1, "zsh: bad assignment\n", 20);
-			else if (ft_strlen(cmd[i]) == 1 && i != 1)
-				write(1, "export: not valid in this context:\n", 35);
-			break ;
-		}
-		if (strchr(cmd[i], '=') != 0 && ft_checkex2(cmd[i], env))
-		{
-			if (!(tmp = (char **)malloc(sizeof(char **) * (tab_len(env) + 2))))
-				return (NULL);
-			j = 0;
-			while(j < tab_len(env) - 1)
+			if (!ft_isalnum(cmd[i][j]) && cmd[i][j] != '=')
 			{
-				if (!(tmp[j] = (char *)malloc(sizeof(char *) * (ft_strlen(env[j]) + 1))))
-					return (NULL);
-				ft_strcpy(tmp[j], env[j]);
-				j++;
-			}
-			if (!(tmp[j] = (char *)malloc(sizeof(char *) * (ft_strlen(cmd[i]) + 1))))
-				return (NULL);
-			ft_strcpy(tmp[j], cmd[i]);
-			if (!(tmp[j + 1] = (char *)malloc(sizeof(char *) * (ft_strlen(env[j]) + 1))))
-				return (NULL);
-			ft_strcpy(tmp[j + 1], env[j]);
-			tmp[j + 2] = NULL;
-			ft_splitdel(&env);
-			if (!(env = (char **)malloc(sizeof(char **) * (tab_len(tmp) + 1))))
-				return (NULL);
-			j = 0;
-			while (tmp[j])
-			{
-				if (!(env[j] = (char *)malloc(sizeof(char *) * (ft_strlen(tmp[j]) + 1))))
-					return (NULL);
-				ft_strcpy(env[j], tmp[j]);
-				j++;
-			}
-			env[j] = NULL;
-			ft_splitdel(&tmp);
-			tmp = NULL;
-		}
-		else
-		{
-			j = 0;
-			while (cmd[i][j] != '=')
-				j++;
-			len = j;
-			j = 0;
-			while (env[j])
-			{
-				if (ft_strncmp(cmd[i], env[j], len) == 0)
+				write(1, "export: not valid in this context: ", 35);
+				j = 0;
+				while (cmd[i][j] != '=' && cmd[i][j])
 				{
-					free(env[j]);
-					if (!(env[j] = (char *)malloc(sizeof(char *) * (ft_strlen(cmd[i]) + 1))))
-						return (NULL);
-					ft_strcpy(env[j], cmd[i]);
-					break ;
+					write(1, &cmd[i][j], 1);
+					j++;
 				}
-				j++;
+				write(1, "\n", 1);
+				j = -1;
+				break ;
+			}
+			j++;
+		}
+		if (j != -1)
+		{
+			if (cmd[i][0] == '=')
+			{
+				if (ft_strlen(cmd[i]) == 1 && i == 1)
+					write(1, "zsh: bad assignment\n", 20);
+				else if (ft_strlen(cmd[i]) == 1 && i != 1)
+					write(1, "export: not valid in this context:\n", 35);
+				break ;
+			}
+			if (strchr(cmd[i], '=') != 0 && ft_checkex2(cmd[i], env))
+			{
+				if (!(tmp = (char **)malloc(sizeof(char **) * (tab_len(env) + 2))))
+					return (NULL);
+				j = 0;
+				while(j < tab_len(env) - 1)
+				{
+					if (!(tmp[j] = (char *)malloc(sizeof(char *) * (ft_strlen(env[j]) + 1))))
+						return (NULL);
+					ft_strcpy(tmp[j], env[j]);
+					j++;
+				}
+				if (!(tmp[j] = (char *)malloc(sizeof(char *) * (ft_strlen(cmd[i]) + 1))))
+					return (NULL);
+				ft_strcpy(tmp[j], cmd[i]);
+				if (!(tmp[j + 1] = (char *)malloc(sizeof(char *) * (ft_strlen(env[j]) + 1))))
+					return (NULL);
+				ft_strcpy(tmp[j + 1], env[j]);
+				tmp[j + 2] = NULL;
+				ft_splitdel(&env);
+				if (!(env = (char **)malloc(sizeof(char **) * (tab_len(tmp) + 1))))
+					return (NULL);
+				j = 0;
+				while (tmp[j])
+				{
+					if (!(env[j] = (char *)malloc(sizeof(char *) * (ft_strlen(tmp[j]) + 1))))
+						return (NULL);
+					ft_strcpy(env[j], tmp[j]);
+					j++;
+				}
+				env[j] = NULL;
+				ft_splitdel(&tmp);
+				tmp = NULL;
+			}
+			else
+			{
+				j = 0;
+				while (cmd[i][j] != '=')
+					j++;
+				len = j;
+				j = 0;
+				while (env[j])
+				{
+					if (ft_strncmp(cmd[i], env[j], len) == 0)
+					{
+						free(env[j]);
+						if (!(env[j] = (char *)malloc(sizeof(char *) * (ft_strlen(cmd[i]) + 1))))
+							return (NULL);
+						ft_strcpy(env[j], cmd[i]);
+						break ;
+					}
+					j++;
+				}
 			}
 		}
 		i++;
@@ -548,6 +574,20 @@ int 				ft_precommande(char *line, char ***env)
 	return (1);
 }
 
+void 				end()
+{
+
+	exit(0);
+}
+
+void 				recovery(int sig)
+{
+	switch(sig){
+		case SIGINT:
+			write(1, "\n$alilin> ", 10);
+	}
+}
+
 int					main(int ac, char **av, char **env)
 {
 	char	*line;
@@ -557,6 +597,8 @@ int					main(int ac, char **av, char **env)
 	(void)av;
 	line = NULL;
 	envi = ft_getenv(env);
+	signal(SIGINT, recovery);
+	signal(EOF, recovery);
 	while (1)
 	{
 		write(1, "$alilin> ", 9);
